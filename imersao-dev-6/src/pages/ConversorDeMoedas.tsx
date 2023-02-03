c15bdfb8cc5054ef79971a5840a4967500cd2d91
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2023-02-03 15:19:20 -0300

    fix: Correção na posição dos títulos

--- imersao-dev-6/src/pages/ConversorDeMoedas.tsx
+++ imersao-dev-6/src/pages/ConversorDeMoedas.tsx
@@ -18,7 +18,7 @@ export function ConversorDeMoedas() {
   return (
     <div className="w-full h-[80vh] flex justify-center items-center flex-col ">
       <img src="https://caelum-online-public.s3.amazonaws.com/assets-imersaodev/Background.png" alt="" className="absolute -z-10 bg-no-repeat bg-center h-screen w-screen" />
-      <div className="text-center p-5">
+      <div className="flex flex-col items-center text-center p-5">
         <h1 className="text-white mb-2 text-3xl">Conversor de Moedas</h1>
         <img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/logo-imersao-aluraflix.svg" alt="" className="w-52 mt-0 mb-3" />
       </div>
