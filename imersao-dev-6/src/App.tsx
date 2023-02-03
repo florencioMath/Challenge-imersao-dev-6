996c9685d8f20a191057bf73cf2d3a1e4845eaf7
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2023-02-03 20:42:31 -0300

    fix: Correção nas rotas

--- imersao-dev-6/src/App.tsx
+++ imersao-dev-6/src/App.tsx
@@ -1,18 +1,16 @@
 import { BrowserRouter } from 'react-router-dom'
 import { Footer } from './components/Footer'
 import { Header } from './components/Header'
-import { Router } from './Router'
+import { AppRoutes } from './routes'
 import './styles/global.css'
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Header />
-        <Router />
-        <Footer />
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Header />
+      <AppRoutes />
+      <Footer />
+    </BrowserRouter>
   )
 }
 
