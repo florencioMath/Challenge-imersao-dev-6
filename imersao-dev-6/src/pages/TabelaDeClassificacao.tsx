231b8854359f2489a25bd9e96f64b7fbf2c5c4a1
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2023-02-15 21:39:36 -0300

    fix: Correção no nome do desafio

--- imersao-dev-6/src/pages/TabelaDeClassificacao.tsx
+++ imersao-dev-6/src/pages/TabelaDeClassificacao.tsx
@@ -85,7 +85,7 @@ export function TabelaDeClassificacao() {
         className='absolute -z-10 bg-no-repeat bg-center h-screen w-screen'
       />
       <div className='flex flex-col items-center text-center p-5'>
-        <h1 className='text-white mb-2 text-3xl'>Tabela de Classificacão</h1>
+        <h1 className='text-white mb-2 text-3xl'>Tabela de Classificação</h1>
         <img
           src='https://www.alura.com.br/assets/img/imersoes/dev-2021/logo-imersao-aluraflix.svg'
           className='w-52 mt-0 mb-3'
