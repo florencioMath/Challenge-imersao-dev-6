3a2294b83573895800ce1af8d7be1a335ad8746d
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2023-02-17 21:26:10 -0300

    feat: Adicionado um link para o florencioMath Github no Header

--- imersao-dev-6/src/components/Header.tsx
+++ imersao-dev-6/src/components/Header.tsx
@@ -3,11 +3,21 @@ import logo from '../assets/logo-imersao-dev6.png';
 
 export function Header() {
   return (
-    <div className='w-auto h-[10vh] bg-backgroundDarkBlue flex items-center justify-between p-8'>
+    <div className='w-auto h-[10vh] bg-backgroundDarkBlue flex items-center justify-between gap-4 p-8'>
       <Link to={'/'}>
         <img src={logo} className='max-w-[240px] max-h-28' />
       </Link>
-      <h1>Header</h1>
+      <p className='text-white'>
+        Criado por{' '}
+        <a
+          href='http://github.com/florencioMath'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='hover:bg-slate-500'
+        >
+          Matheus Florêncio
+        </a>
+      </p>
     </div>
   );
 }
