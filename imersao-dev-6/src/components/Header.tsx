0bc2f8776b0ec47f405c26164100404a65a91de1
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2023-02-17 21:20:19 -0300

    fix: Correção no height e width do Header e Footer

--- imersao-dev-6/src/components/Header.tsx
+++ imersao-dev-6/src/components/Header.tsx
@@ -5,7 +5,7 @@ export function Header() {
   return (
     <div className='w-auto h-[10vh] bg-backgroundDarkBlue flex items-center justify-between p-8'>
       <Link to={'/'}>
-        <img src={logo} className='w-1/3 max-h-44' />
+        <img src={logo} className='max-w-[240px] max-h-28' />
       </Link>
       <h1>Header</h1>
     </div>
